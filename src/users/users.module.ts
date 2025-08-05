ed571dbb32f38dc189dcab9562ad033dca67eb78
SDSamarasinghe <it20605902@my.sliit.lk>
2025-08-05 11:39:00 +0530

    feat: add Pet schema to Users module for improved data management

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User, UserSchema } from './schemas/user.schema';
+import { Pet, PetSchema } from '../pets/schemas/pet.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Pet.name, schema: PetSchema }
+    ])
   ],
   controllers: [UsersController],
   providers: [UsersService],
