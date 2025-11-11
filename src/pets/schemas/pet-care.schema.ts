0152d77edb3c38e14d13796e90f8ba9bd383d709
SDSamarasinghe <it20605902@my.sliit.lk>
2025-11-11 17:46:33 +0530

    feat: Add additional fields to PetCare schema for enhanced pet care details

--- src/pets/schemas/pet-care.schema.ts
+++ src/pets/schemas/pet-care.schema.ts
@@ -9,6 +9,30 @@ export class PetCare {
   petId: Types.ObjectId;
 
   @Prop()
+  personalityPhobiasPreferences: string; // Personality, any Phobias and Preferences
+
+  @Prop()
+  typeOfFood: string; // Type of food
+
+  @Prop()
+  dietFoodWaterInstructions: string; // Diet - Food, Water instructions, Quantity and Locations
+
+  @Prop({ enum: ['Yes', 'No'] })
+  anyHistoryOfBiting: string; // Any History of Biting
+
+  @Prop()
+  locationOfStoredPetFood: string; // Location of Stored Pet Food and Treats
+
+  @Prop()
+  litterBoxLocation: string; // Litter Box Location and Additional Litter Location
+
+  @Prop()
+  locationOfPetCarrier: string; // Location of Pet Carrier
+
+  @Prop()
+  anyAdditionalInfo: string; // Any Additional Info
+
+  @Prop()
   careInstructions: string; // Care instructions
 
   @Prop()
