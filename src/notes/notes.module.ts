555547ec0558a44e72715249f48a6ab276d0fd3a
SDSamarasinghe <it20605902@my.sliit.lk>
2025-11-16 11:41:29 +0530

    feat: Import EmailModule in NotesModule for email functionality

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -4,6 +4,7 @@ import { NotesController } from './notes.controller';
 import { NotesService } from './notes.service';
 import { Note, NoteSchema } from './schemas/note.schema';
 import { User, UserSchema } from '../users/schemas/user.schema';
+import { EmailModule } from '../email/email.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { User, UserSchema } from '../users/schemas/user.schema';
       { name: Note.name, schema: NoteSchema },
       { name: User.name, schema: UserSchema },
     ]),
+    EmailModule,
   ],
   controllers: [NotesController],
   providers: [NotesService],
